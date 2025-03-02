1b7ed1318806803c44238b91131a7b327b67a1f8
isiah-a <isiaha478@gmail.com>
2025-03-02 18:34:30 -0500

    started

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,10 +1,35 @@
 package com.github.zipcodewilmington;
 //Isiah Armstrong
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    public static void main(String[] args){
+
+        runGame();
+        WordGuess();
+
+
+    }
+
+    public static void runGame() {
+        System.out.println("Let's play Hangman!");
+        Scanner scanner = new Scanner(System.in);
+    }
+    public static void WordGuess(char s){
+       String words = Arrays.toString(new String[]{"cat", "dog", "bog", "cut"});
+       char[] array = words.toCharArray();
+        System.out.println(array[6]);
+        int randomNum = array.length - 1;
+
+    }
+
+
 }
